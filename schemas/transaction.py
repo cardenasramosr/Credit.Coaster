d6b0b661f7c354c99b40dd9a4e4aeb9ef5691cc8
Ricardo C <cardenasramosr@gmail.com>
2023-03-07 19:40:17 -0600

    Comments schemas

--- schemas/transaction.py
+++ schemas/transaction.py
@@ -3,5 +3,5 @@ from schemas.card import Card
 
 class Transaction:
     def __init__(self, card: Card, amount: float):
-        self.card = card
-        self.amount = amount
+        self.card = card #Se llama la funcion card y se agrega el número de tarjeta
+        self.amount = amount #Se llama la funcion amount y se agrega el monto 
--- schemas/user.py
+++ schemas/user.py
@@ -1,5 +1,5 @@
 class User:
     def __init__(self, name: str, birthdate: str, phone: str):
-        self.name = name
-        self.birthdate = birthdate
-        self.phone = phone
+        self.name = name #Se agrega el nombre del usuario
+        self.birthdate = birthdate #Se agrega la fecha de nacimiento del usuario
+        self.phone = phone #Se agrega el telefono del usuario
