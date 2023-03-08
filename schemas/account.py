458521ec9a3b962d6c468e681352d288f3381434
Ricardo C <cardenasramosr@gmail.com>
2023-03-07 19:44:29 -0600

    Cambios comments

--- schemas/account.py
+++ schemas/account.py
@@ -3,5 +3,5 @@ from schemas.user import User
 
 class Account:
     def __init__(self, user: User, balance: float):
-        self.user = user
-        self.balance = balance
+        self.user = user #Se llama la funcion user y se guarda
+        self.balance = balance #Se guarda el balance de la cuenta 
--- schemas/card.py
+++ schemas/card.py
@@ -7,7 +7,7 @@ from schemas.account import Account
 class Card:
     def __init__(self, account: Account, nip: int):
         now = datetime.datetime.now()
-        self.account = account
-        self.nip = nip
+        self.account = account #Se llama la funcion account y se agrega el valor cuenta
+        self.nip = nip #Se guarda el numero del nip de la tarjeta
         self.number = random.randint(4400_000_000_000, 4500_000_000_000) #Genera un numero random para la tarjeta
-        self.expiration = datetime.datetime(year=now.year + 5, month=now.month, day=1)
+        self.expiration = datetime.datetime(year=now.year + 5, month=now.month, day=1) #Se genera una fecha de hoy a 5 años con el mismo mes y mismo dia
